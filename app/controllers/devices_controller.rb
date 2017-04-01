816d019f694096eb200314d66116da3f8eba25de
Stephen Wetzel <stephen.wetzel1@gmail.com>
2017-04-01 01:43:56 -0400

    Basic controllers

---
+++ app/controllers/devices_controller.rb
@@ -0,0 +1,12 @@
+class DevicesController < ApplicationController
+  def index
+    render json: Device.all.to_json, status: :ok
+  end
+
+  def show
+    render json: Device.first(device_guid: params[:device_guid]).to_json, status: :ok
+  end
+
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  scope '/api' do
+    scope '/v1' do
+      scope 'devices' do
+        get '/' => 'devices#index'
+        get '/:device_guid' => 'devices#show'
+      end
+    end
+  end
 end
