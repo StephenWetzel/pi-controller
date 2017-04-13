994961eed4f61075e112ef621a9b4b1670203357
Stephen Wetzel <stephen.wetzel1@gmail.com>
2017-04-12 20:39:42 -0400

    make event responses prettier

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -51,13 +51,51 @@ class DevicesController < ApplicationController
         state_code: workflow[:to_state]
       }
       Rails.logger.info "Broadcasted to #{connection_count} connections"
-      render json: EventLog.first(event_log_id: event_log_id), status: :ok
     end
+    sleep(1)
+    last_event = EventLog.reverse_order(:request_dt).first
+
+    render json: {
+      response: last_event[:response],
+      age: age(last_event[:request_dt]).compact,
+      id: last_event[:event_log_id]
+    }, status: :ok
   end
 
   private
 
   def device_params
     params.permit(:device_guid, :device_name, :device_description, :workflow_name, :state_code)
+  end
+
+  SECONDS_IN_MINUTE = 60
+  SECONDS_IN_HOUR = 60 * 60
+  SECONDS_IN_DAY = 60 * 60 * 24
+  SECONDS_IN_YEAR = 60 * 60 * 24 * 365
+
+  # Returns negative seconds for future dates
+  def age(from_dt)
+    to_dt = Time.current
+
+    total_seconds = (to_dt - from_dt).to_i
+    Rails.logger.info "From #{from_dt}, To #{to_dt}, seconds: #{total_seconds}"
+    seconds = total_seconds
+    if total_seconds > SECONDS_IN_YEAR
+      years = seconds / SECONDS_IN_YEAR
+      seconds -= (years * SECONDS_IN_YEAR)
+    end
+    if total_seconds > SECONDS_IN_DAY
+      days = seconds / SECONDS_IN_DAY
+      seconds -= (days * SECONDS_IN_DAY)
+    end
+    if total_seconds > SECONDS_IN_HOUR
+      hours = seconds / SECONDS_IN_HOUR
+      seconds -= (hours * SECONDS_IN_HOUR)
+    end
+    if total_seconds > SECONDS_IN_MINUTE
+      minutes = seconds / SECONDS_IN_MINUTE
+      seconds -= (minutes * SECONDS_IN_MINUTE)
+    end
+    return {years: years, days: days, hours: hours, minutes: minutes, seconds: seconds}
   end
 end
